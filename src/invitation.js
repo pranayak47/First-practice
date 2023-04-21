336000af174a54b6f8ae97a084dfa6cd5b1fdeec
pranayak47 <kumarpranay719@gmail.com>
2023-04-21 08:32:48 +0530

    Invitation

---
+++ src/invitation.js
@@ -0,0 +1,14 @@
+export default function Invitation(){
+    return(
+        <div>
+            <p> Subject : Birthday party Invitation </p>
+            <p> To : jaGdish@gmail.com</p>
+            <p> Hi , jaGdish</p>
+            <p>  I am having a birthday party next Friday at my Home . Would you like to come ? It will be fun . Lots of people from my school are coming . you know some of them - Ritu , Saurabh , Kartik .</p>
+            <p>  My house is behind our school , near Green field Avenue</p>
+            <p>  I hope you will come and see you soon .</p>
+            <p>  from ,</p>
+            <p>  JaGdish</p>
+        </div>
+    )
+}
