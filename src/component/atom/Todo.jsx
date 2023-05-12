49131ea24f872aea99bc6f5cbf88a47846d80416
pranayak47 <kumarpranay719@gmail.com>
2023-05-13 01:27:21 +0530

    codes for To  List

---
+++ src/component/atom/Todo.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './Todo.css'
+
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleAddTodo = () => {
+        if (inputValue.trim() !== '') {
+            setTodos([...todos, inputValue.trim()]);
+            setInputValue('');
+        }
+    };
+
+    const handleDeleteTodo = (index) => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
+
+    return (
+        <div>
+            <h1>To-Do List</h1>
+            <input type="text" value={inputValue} onChange={handleInputChange} />
+            <button onClick={handleAddTodo}>Add</button>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>
+                        {todo}
+                        <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default TodoList;
---
+++ src/component/molecule/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from '../atom/Todo';
+
+function Main() {
+    return (
+        <div>
+            <TodoList/>
+        </div>
+    );
+}
+
+export default Main;
